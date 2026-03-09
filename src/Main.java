814a2453ff9ca63514e02eb3823d0dc39808e7bf
TerryDavisDevine <oneill-f27@ulster.ac.uk>
2026-03-09 16:52:48 +0000

    removing testing statements

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+//created by TerryDavisDevine
 import java.util.Locale;
 import java.util.Scanner;
 import java.io.File;
@@ -8,18 +9,23 @@ public class Main{
     String qfilepath = "";
     String afilepath="";
     Scanner keyboard = new Scanner(System.in);
+    //getting text from text files and adding each new line as an element of an array
     public String[] getText(String filepath){
         try {
+            //creating a new file and scanner object for retrieving text
             File questions = new File(filepath);
             Scanner reader = new Scanner(questions);
             int count =0;
+            //counting how many lines the text file is to set the array size
             while(reader.hasNextLine()){
                 reader.nextLine();
                 count++;
             }
+            //creating a new scanner object as the last one has already went through all of its lines
             reader = new Scanner(questions);
             String[] questionArray = new String[count];
             count = 0;
+            //adding text lines as elements
             while(reader.hasNextLine()){
                 questionArray[count] = reader.nextLine();
                 count++;
@@ -28,17 +34,21 @@ public class Main{
         }catch(Exception e){
             System.out.println(e);
         }
+        //setting a final return statement to stop errors from not having one at the end of the method, will only execute if the try block is not
         return new String[]{"error"};
     }
+    //method to quiz the users
     public void askQ(String[] questions, String[] answers){
         String answer;
         int score=0;
+        //checking if there are any questions
         if(questions.length == 0){
             System.out.println("There are currently no questions under this topic, please create some questions before attempting the test");
         }else {
             for (int i = 0; i < questions.length; i++) {
                 System.out.println(questions[i]);
                 answer = keyboard.nextLine();
+                //checking if answer is correct
                 if (answer.toLowerCase().equals(answers[i].toLowerCase(Locale.ROOT))) {
                     System.out.println("Correct");
                     score++;
@@ -49,17 +59,8 @@ public class Main{
             System.out.println("Score: " + score + "/" + questions.length);
         }
     }
+    //method to add questions to a topic
     public void addQuestion(){
-        try(FileWriter newq = new FileWriter(qfilepath,true)){
-            newq.write("\r\n");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        try(FileWriter newq = new FileWriter(afilepath,true)){
-            newq.write("\r\n");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
         while(true){
             System.out.println("What question would you like to add?");
             String Question = keyboard.nextLine();
@@ -82,8 +83,9 @@ public class Main{
             }
         }
     }
+    //method to provide a menu
     public void start(Main test) {
-        System.out.println("Would you like to (a create a new topic (b continue to existing topics (c shutdown");
+        System.out.println("Would you like to:\n(a) create a new topic\n(b) continue to existing topics\n(any other key) shutdown");
         String decision = keyboard.nextLine().toLowerCase(Locale.ROOT);
         switch (decision) {
             case "a":
@@ -95,36 +97,43 @@ public class Main{
                 File files = new File("resources/");
                 File[] allFiles = files.listFiles();
                 String topics = "";
-                for (int i = 0; i < allFiles.length; i++) {
-                    if (allFiles[i].isDirectory()) {
-                        if(i== allFiles.length-1){
-                            topics+=allFiles[i].getName();
-                        }else{
-                            topics += allFiles[i].getName() + ", ";
+                if(allFiles.length > 0) {
+                    for (int i = 0; i < allFiles.length; i++) {
+                        if (allFiles[i].isDirectory()) {
+                            if (i == allFiles.length - 1) {
+                                topics += "or "+allFiles[i].getName();
+                            } else {
+                                topics += allFiles[i].getName() + ", ";
+                            }
                         }
                     }
+                    System.out.print("What subject would you like to study for " + topics + "?\n");
+                    String choice = keyboard.nextLine().toUpperCase(Locale.ROOT);
+                    qfilepath = "resources/" + choice + "/questions.txt";
+                    afilepath = "resources/" + choice + "/answers.txt";
+                    System.out.print("What would you like to do: (a begin quiz (b add a question\n");
+                    String ans = keyboard.nextLine().toLowerCase(Locale.ROOT);
+                    if (ans.equals("a")) {
+                        test.askQ(test.getText(qfilepath), test.getText(afilepath));
+                    } else if (ans.equals("b")) {
+                        addQuestion();
+                    }
+                    test.start(test);
+                    break;
+                }else{
+                    System.out.println("There are currently no topics in available, please add some topics and try again");
                 }
-                System.out.println("What subject would you like to study for " + topics + "?\n");
-                String choice = keyboard.nextLine().toUpperCase(Locale.ROOT);
-                qfilepath = "resources/" + choice + "/questions.txt";
-                afilepath = "resources/" + choice + "/answers.txt";
-                System.out.print("What would you like to do: (a begin quiz (b add a question (press any other key for exit)\n");
-                String ans = keyboard.nextLine().toLowerCase(Locale.ROOT);
-                if (ans.equals("a")) {
-                    test.askQ(test.getText(qfilepath), test.getText(afilepath));
-                } else if (ans.equals("b")) {
-                    addQuestion();
-                }
-                test.start(test);
-                break;
             default:
-                System.out.println("Program shutting down");
+                //exiting program
+                System.out.println("Program shutting down, goodbye");
                 exit(0);
         }
+        //recursion, restarting method so the user can keep using the program, the user can exit on the next iteration if they want
         start(test);
     }
 
     public static void main(String[] args){
+        //entry point to program, establishing a new instance of main class to use non-static methods
         Main test = new Main();
         test.start(test);
     }
--- src/createTopic.java
+++ src/createTopic.java
@@ -8,8 +8,19 @@ public class createTopic {
     public void createDir(){
         File folder = new File("resources/"+dirName);
         if(folder.mkdir()){
-            System.out.println(folder.getAbsolutePath());
+            folder.getAbsolutePath();
             System.out.println("New topic created");
+            File newQFile = new File(folder.getAbsolutePath()+"/questions.txt");
+            File newAFile = new File(folder.getAbsolutePath()+"/answers.txt");
+            try {
+                if (newQFile.createNewFile() && newAFile.createNewFile()){
+                    System.out.println("Questions file created succesfully");
+                }else{
+                    System.out.println("Error when creating quiz files, please check if the directory already contains these");
+                }
+            }catch(Exception e){
+                System.out.println(e);
+            }
         }else{
             System.out.println("An error occured, please check if the folder already exists");
         }
